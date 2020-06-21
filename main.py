d6442739f8da2ea75d32a77387b501d04a8e2e75
adi-ua <abanerjee@email.arizona.edu>
2020-06-20 21:01:25 -0700

    * Moved LA converion to happen along with RGBA conversion so double work is avoided. * Optimised process signature to change only alpha value for brightness > threshold and otherwise change both channels appropriately to opaque black. * The above changes speed up processing by approx. 20%. * The output also now resembles a sprite rather than an image with a white background. * Removed non-normalized plane creation in shadow crusher because it wasn't being used. * Shadow crusher now splits into the two LA planes. We can make this more general but either way it needs more comments. * Added explicit exit with code 1 to open/save image failure so the program terminates correctly.

--- main.py
+++ main.py
@@ -25,24 +25,25 @@ def input_image():
             ("PNG files",
              "*.jpg")))
     if root.filename:
-        im = Image.open(root.filename).convert('LA')
+        im = Image.open(root.filename)
     else:
         print("No image was chosen")
+        exit(1)
     return im
 
 
-def process_image_to_RGBA_array(im):
+def process_image_to_LA_array(im):
     """
-    This function converts the image to RGBA mode if it wasn't already and
+    This function converts the image to LA mode if it wasn't already and
     then converts the image into its nd array representation.
 
     Args:
-        im (PIL Image): The image to convert to an nd array in RGBA format
+        im (PIL Image): The image to convert to an nd array in LA format
     Returns:
-        Numpy array: An nd numpy array. In this case each pixel position has a depth of 4 including the alpha channel.
+        Numpy array: An nd numpy array. In this case each pixel position has a depth of 2 including the alpha channel.
     """
 
-    im = im.convert("RGBA")
+    im = im.convert("LA")
     im_arr = np.array(im)
 
     return im_arr
@@ -52,7 +53,7 @@ def process_signature(im_arr):
     """
     This function takes the numpy array of an image and goes through it pixel by pixel.
     It checks if a pixel is above a certain threshold value. If it is, the pixel is turned
-    to a pure white pixel. Else, it gets converted to dark grey.
+    to a transparent pixel. Else, it gets converted to dark grey.
 
     Args:
         im_arr (numpy array): Numpy array that needs to be edited
@@ -67,12 +68,10 @@ def process_signature(im_arr):
     for i in range(rows):
         for j in range(columns):
             if im_arr[i][j][0] > threshold:
-                im_arr[i][j] = 255
+                im_arr[i][j][1] = 0  # if brightness > threshold then assume it is part of the background and make it transparent.
             else:
                 im_arr[i][j][0] = 15
-                im_arr[i][j][1] = 15
-                im_arr[i][j][2] = 15
-                im_arr[i][j][3] = 255
+                im_arr[i][j][1] = 255  # Else make opaque with black value of 15
     return im_arr
 
 
@@ -82,10 +81,10 @@ def save_to_file(im_arr):
     whatever the user inputs in the file dialog box.
 
     Args:
-        im_arr (Numpy array): The nd array representing the image. Make sure it can be converted to RGBA.
+        im_arr (Numpy array): The nd array representing the image. Make sure it can be converted to LA.
     """
 
-    im = Image.fromarray(im_arr, mode="RGBA")
+    im = Image.fromarray(im_arr, mode="LA")
 
     filename = filedialog.asksaveasfile(mode='wb', defaultextension=".png")
 
@@ -94,32 +93,31 @@ def save_to_file(im_arr):
         im.show()
     else:
         print("No save location was chosen")
+        exit(1)
 
 
 def shadow_crusher(img):
     """
-    This function takes the numpy array of an image and removes the shadows.
-    It returns a normalised image in the form of an array
+    This function takes the numpy LA array of an image and removes the shadows.
+    It returns a normalised image in the form of an array.
+
     Args:
         img (numpy array): Image for which that shadow needs to be removed
 
     Returns:
         [numpy array]: Normalized Image with the shadows removed or flattened.
     """
-    rgb_planes = cv2.split(img)
+    la_planes = cv2.split(img)
 
-    result_planes = []
     result_norm_planes = []
-    for plane in rgb_planes:
+    for plane in la_planes:
         dilated_img = cv2.dilate(plane, np.ones((7, 7), np.uint8))
         bg_img = cv2.medianBlur(dilated_img, 21)
         diff_img = 255 - cv2.absdiff(plane, bg_img)
         norm_img = cv2.normalize(
             diff_img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
-        result_planes.append(diff_img)
         result_norm_planes.append(norm_img)
 
-    result = cv2.merge(result_planes)
     result_norm = cv2.merge(result_norm_planes)
 
     return result_norm
@@ -134,7 +132,7 @@ def main():
     """
 
     im = input_image()
-    im_arr = process_image_to_RGBA_array(im)
+    im_arr = process_image_to_LA_array(im)
     im_arr = shadow_crusher(im_arr)
     output_im_array = process_signature(im_arr)
     save_to_file(output_im_array)
