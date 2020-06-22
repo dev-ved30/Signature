31e35adc20989ba5c69afc815a0da5755affdb75
Ved Shah <54833781+dev-ved30@users.noreply.github.com>
2020-06-22 20:17:38 +0530

    Feature 1 (#3)
    * Added pixel by pixel processing.
    * Manipulated image now opens up after computation
    * Added `Shadow Crusher` function to remove shadows from an image with in detail documentation
    * Updated documentation
    * Updated `README` 
    * Moved LA conversion to happen with RGBA conversion for simplified work
    * The output also now resembles a sprite rather than an image with a white background.
    * Added explicit exit with code 1 to open/save image failure so the program terminates correctly.
    
    
    Co-authored-by: adi-ua <abanerjee@email.arizona.edu>
    Co-authored-by: Aditya Banerjee <55006642+Adi-UA@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # Signature
+
+
+## Using Locally
+
+### Before Running (Assuming you have pip and git)
+
+On Windows in `cmd`:
+
+* Run `pip install numpy`
+* Run `pip install Pillow`
+* Run `pip install opencv-python`
+
+On Debian-based Linux in terminal:
+* Run `pip install numpy`
+* Run `pip install Pillow`
+* Run `sudo apt-get install python3.6-tk`
+* Run `sudo apt-get install python3-opencv`
+
+*Note:* For develoment and testing, we used `Tkinter 8.6`, `Pillow 7.1.2`, `Open CV 4.2.0`,`Python 3.7.4` and `Numpy 1.18.4`.
+
+### Running
+
+In your terminal, navigate to the project folder you cloned and run:
+
+`python main.py`
+
+The command assumes python3 is aliased to python. On Windows if you only have Python 3.x installed the aliasing should be done for you, but on Linux you would have be more explicit depending on the distro and its version. So, if the command above fails try:
+
+`python3 main.py`
+
+We do plan on launching an `exe` for the this utility in a future release!
+
+## Found Bugs?
+
+We would appreciate any support in the form of bug reports in order to provide the best possible experience. Bugs can be reported in the `Issues` tab
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import Tk
 from tkinter import filedialog
 from PIL import Image
 import numpy as np
+import cv2
 
 
 def input_image():
@@ -27,31 +28,53 @@ def input_image():
         im = Image.open(root.filename)
     else:
         print("No image was chosen")
+        exit(1)
     return im
 
 
-
-def process_image_to_RGBA_array(im):
+def process_image_to_LA_array(im):
     """
-    This function converts the image to RGBA mode if it wasn't already and
+    This function converts the image to LA mode if it wasn't already and
     then converts the image into its nd array representation.
 
     Args:
-        im (PIL Image): The image to convert to an nd array in RGBA format
+        im (PIL Image): The image to convert to an nd array in LA format
     Returns:
-        Numpy array: An nd numpy array. In this case each pixel position has a depth of 4 including the alpha channel.
+        Numpy array: An nd numpy array. In this case each pixel position has a depth of 2 including the alpha channel.
     """
 
-    im = im.convert("RGBA")
+    im = im.convert("LA")
     im_arr = np.array(im)
 
     return im_arr
 
 
 def process_signature(im_arr):
-    # TODO - This function needs to be implemented
-    return im_arr
+    """
+    This function takes the numpy array of an image and goes through it pixel by pixel.
+    It checks if a pixel is above a certain threshold value. If it is, the pixel is turned
+    to a transparent pixel. Else, it gets converted to dark grey.
 
+    Args:
+        im_arr (numpy array): Numpy array that needs to be edited
+
+    Returns:
+        numpy array: Edited numpy array
+    """
+    threshold = 204
+    shape = im_arr.shape
+    rows = shape[0]
+    columns = shape[1]
+    for i in range(rows):
+        for j in range(columns):
+            if im_arr[i][j][0] > threshold:
+                # if brightness > threshold then assume it is part of the background and make it transparent.
+                im_arr[i][j][1] = 0
+            else:
+                im_arr[i][j][0] = 15
+                # Else make opaque with black value of 15
+                im_arr[i][j][1] = 255
+    return im_arr
 
 
 def save_to_file(im_arr):
@@ -60,18 +83,70 @@ def save_to_file(im_arr):
     whatever the user inputs in the file dialog box.
 
     Args:
-        im_arr (Numpy array): The nd array representing the image. Make sure it can be converted to RGBA.
+        im_arr (Numpy array): The nd array representing the image. Make sure it can be converted to LA.
     """
 
-    im = Image.fromarray(im_arr, mode="RGBA")
+    im = Image.fromarray(im_arr, mode="LA")
 
     filename = filedialog.asksaveasfile(mode='wb', defaultextension=".png")
 
     if filename:
         im.save(filename)
+        im.show()
     else:
         print("No save location was chosen")
+        exit(1)
 
+
+def shadow_crusher(img):
+    """
+    This function takes the numpy LA array of an image and removes the shadows.
+    It returns a normalised image in the form of an array.
+
+    Steps:
+
+    Split: Changes mutlichannel image to 2 seperate single channel Images
+
+    Dilation: This uses pixel clusters to dilate the image. It replaces the 
+    parts of the image where content appears with white squares that are 7 by 7
+    pixels which helps remove the content while preserving the shadows
+
+    Median Blur: This blurs or smoothens the dilated image. As a result,
+    the places from where the content is removed does not appear to be 
+    pixelated. The aperture size is 21.
+
+    Absolute Difference: This compares the orignal image with the blurred one,
+    pixel by pixel. If the difference between those is 255, which it would be for
+    pixels containing content, diff_image is assigned the value 0, which would make
+    those pixels black. Else, the pixel will get a whiter shade due to the difference.
+
+    Normalize: Normalizes the image to use the entire 8 bit range. alpha is the lower
+    boundary while beta is the upper one. It makes light parts of the image lighter and
+    dark part darker. This makes it easier to work with higher thresholds in the 
+    process_signature function.
+
+    Merge: Merges the different channels of the image into a single image.
+
+    Args:
+        img (numpy array): Image for which that shadow needs to be removed
+
+    Returns:
+        [numpy array]: Normalized Image with the shadows removed or flattened.
+    """
+    la_planes = cv2.split(img)
+
+    result_norm_planes = []
+    for plane in la_planes:
+        dilated_img = cv2.dilate(plane, np.ones((7, 7), np.uint8))
+        bg_img = cv2.medianBlur(dilated_img, 21)
+        diff_img = 255 - cv2.absdiff(plane, bg_img)
+        norm_img = cv2.normalize(
+            diff_img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+        result_norm_planes.append(norm_img)
+
+    result_norm = cv2.merge(result_norm_planes)
+
+    return result_norm
 
 
 def main():
@@ -81,9 +156,10 @@ def main():
     It reads an image, preprocesses it, then processes it and finally
     saves it to a file.
     """
-    
+
     im = input_image()
-    im_arr = process_image_to_RGBA_array(im)
+    im_arr = process_image_to_LA_array(im)
+    im_arr = shadow_crusher(im_arr)
     output_im_array = process_signature(im_arr)
     save_to_file(output_im_array)
 
