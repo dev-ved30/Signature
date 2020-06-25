1728e4cb625e069d3c72067eb9efd0dc29eb0a52
dev-ved30 <vedshah.email@gmail.com>
2020-06-25 17:44:51 +0530

    changed blur parameter to 4

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ def shadow_crusher(img):
 def alias(img):
     img_thresh = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)[1]
     img_blur = cv2.pyrUp(img_thresh)
-    img_blur = cv2.medianBlur(img_blur, 3)
+    img_blur = cv2.medianBlur(img_blur, 4)
     img_blur = cv2.pyrDown(img_blur)
     return img_blur
 
