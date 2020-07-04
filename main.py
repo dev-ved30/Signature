* shadow crusher is now more streamlined and less redundant. * Bug with alpha chanel normalization has been fixed * alpha channel is being thresholed properly in the threshold function now * Documentation need to be updated

main.py
@@ -49,21 +49,24 @@ def process_image_to_LA_array(im):
 
 def threshold_image(im_arr, threshold=204):
     """
-    This function accepts an image array and sets all pixels in the grescale channel below the threshold value
-    to black.The blackend pixels' alpha channel is set to 255 as well.
+    This function accepts an image array and sets all pixels in the greyscale channel below the threshold value
+    to black. The blackend pixels' alpha channel is set to 255 and the rest of the alpha channel is set to 0.
 
     Args:
-        im_arr (Numpy ndarray): The array representing the image in LA. IMPORTANT NOTE: The image must have it's entire alpha channel set
-        to 0 before being passed to this function.
+        im_arr (Numpy ndarray): The array representing the image in LA.
 
         threshold (int, optional): Pixels in the greyscale whose value is below this are set to 0. Defaults to 204.
 
     Returns:
         Numpy array: The thresholded image.
     """
+    im_arr[:, :, 1] = 0  # Setting entire alpha channel to 0
+
     l_channel = im_arr[:, :, 0]  # Extract the greyscale channel
+
     # Find positions of pixels whose value is below threshold
     row_indices, col_indices = np.where(l_channel < threshold)
+
     # Set to black with no transparency
     im_arr[row_indices, col_indices] = [0, 255]
     return im_arr
@@ -124,20 +127,15 @@ def shadow_crusher(im_arr):
     Returns:
         [numpy array]: Normalized Image with the shadows removed or flattened.
     """
-    la_planes = cv.split(im_arr)
+    l_plane = im_arr[:, :, 0]
 
-    result_norm_planes = []
-    for plane in la_planes:
-        dilated_img = cv.dilate(plane, np.ones((7, 7), np.uint8))
-        bg_img = cv.medianBlur(dilated_img, 21)
-        diff_img = 255 - cv.absdiff(plane, bg_img)
-        norm_img = cv.normalize(
-            diff_img, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_8UC1)
-        result_norm_planes.append(norm_img)
+    dilated_img = cv.dilate(l_plane, np.ones((7, 7), np.uint8))
+    bg_img = cv.medianBlur(dilated_img, 21)
+    diff_img = 255 - cv.absdiff(l_plane, bg_img)
+    cv.normalize(
+        diff_img, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_8UC1)
 
-    result_norm = cv.merge(result_norm_planes)
-
-    return result_norm
+    return im_arr
 
 
 def alias(im_arr):