8715f38dba97eae70a3e5434f96057fd746b16c3
dev-ved30 <vedshah.email@gmail.com>
2020-07-17 17:29:29 +0530

    Page and image is updated every time the slider value changes

--- eel/main.py
+++ eel/main.py
@@ -9,26 +9,27 @@ def process():
     im_la = process_image_to_LA_array(im)
     im_shadow_crush = shadow_crusher(im_la)
     save_shadow_crush(im_shadow_crush)
+    print("Saved the shadow crushed function")
     im_thresh = threshold_image(im_shadow_crush)
     im_alias = alias(im_thresh)
     save_to_temp(im_alias)
+    print("Inital image stored")
 
 @eel.expose
 def change_thresh(thresh):
-    print("hi")
-    print(thresh)
-    print(type(thresh))
     im = input_crushed_img()
     im_la = process_image_to_LA_array(im)
     im_thresh = threshold_image(im_la, int(thresh))
     im_alias = alias(im_thresh)
     save_to_temp(im_alias)
+    print("Threshold change completed. Threshold set to", thresh)
 
 @eel.expose
 def save_final_image():
     im = input_temp_img()
     im_la = process_image_to_LA_array(im)
     save_to_file(im_la)
+    print("Final image stored")
 
     
 
--- eel/web/main.js
+++ eel/web/main.js
@@ -7,6 +7,7 @@ function reprocess() {
 	var threshold = document.getElementById('thresh').value;
 	eel.change_thresh(threshold);
 	console.log('Threshold re-processing is complete');
+	location.reload();
 }
 function save_img() {
 	eel.save_final_image();
