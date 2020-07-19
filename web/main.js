b020ca216d8a4b3cc0cbf93f156a3563e42f1a6b
dev-ved30 <vedshah.email@gmail.com>
2020-07-19 22:55:31 +0530

    implementation with cookies

--- web/main.js
+++ web/main.js
@@ -1,5 +1,3 @@
-var x = 10;
-
 function process_img() {
 	eel.process_img();
 	console.log('Initial processing is complete');
@@ -8,18 +6,16 @@ function process_img() {
 function reprocess() {
 	var threshold = document.getElementById('thresh').value;
 	eel.change_thresh(threshold);
+	document.cookie = document.getElementById('thresh').value;
 	console.log('Threshold re-processing is complete. Threshold is set to ' + threshold);
 	location.reload();
 }
 function save_img() {
 	eel.save_final_image();
-	console.log('Final image is now saved');
 }
-function get_thresh() {
-	var x = 200;
-	eel.get_thresh_val()(function(ret) {
-		console.log(ret);
-		x = ret;
-	});
-	return x;
-}
+/* function get_cookie_value() {
+	var x = document.cookie;
+	var val = x.split(';');
+	console.log(val[1]);
+	return val[1];
+} */
