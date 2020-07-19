446303b840c14acf2a66386a1e813ac7bf04136b
dev-ved30 <vedshah.email@gmail.com>
2020-07-19 23:10:05 +0530

    fixed final bug with slider

--- web/main.js
+++ web/main.js
@@ -6,16 +6,10 @@ function process_img() {
 function reprocess() {
 	var threshold = document.getElementById('thresh').value;
 	eel.change_thresh(threshold);
+	document.getElementById('img').src = 'temp/.temp.png';
 	document.cookie = document.getElementById('thresh').value;
 	console.log('Threshold re-processing is complete. Threshold is set to ' + threshold);
-	location.reload();
 }
 function save_img() {
 	eel.save_final_image();
 }
-/* function get_cookie_value() {
-	var x = document.cookie;
-	var val = x.split(';');
-	console.log(val[1]);
-	return val[1];
-} */
